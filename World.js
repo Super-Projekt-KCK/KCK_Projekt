2abb63d4a21b9cbc871684547af3a803ab365f2b
MatPiw <mateusz.piwecki@wp.pl>
2014-10-19 00:53:29 +0200

    Wstępne rysowanie grafów

--- World.js
+++ World.js
@@ -1,7 +1,3 @@
-/**
- * Created by Mateusz on 17-10-2014.
- */
-
 //tworzenie tablicy 2-wymiarowej
 function createArray(x, y) {
 
@@ -30,17 +26,93 @@ function printArray(name) {
     }
 }
 
+//macierz sąsiedztwa dla grafu z x wierzchołkami
 function fillArray() {
-    var temp;
+    var maxEdges = 12;          //max liczba krawędzi
+    var edges = 0;
     for (var i = 0; i < map.length; i++) {
-        for (var j = 0; j < map[i].length; j++) {
-            if (Math.random() < 0.4)
+        for (var j = i+1; j < map[i].length; j++) {
+            if ((Math.random() < 0.5) || (edges >= maxEdges))
                 map[i][j] = 0;
-            else
+            else {
                 map[i][j] = 1;
+                edges++;
+            }
+        }
+        for (var j = 0; j <= i; j++) {
+            map[i][j] = 0;
         }
     }
+
 }
+
+var coords = createArray(2, map.length);        //współrzędne wierzchołków grafu na canvas
+var width = 300;            //szerokość i wysokość canvas
+var height = 150;
+
+function makeGraph() {
+    for (var i = 0; i < map.length; i++) {            //losowanie współrzędnych grafu
+        coords[0][i] = Math.floor(Math.random() * width);
+        coords[1][i] = Math.floor(Math.random() * height);
+        document.getElementById("coords").innerHTML += coords[0][i] +"," + coords[1][i] + " ";
+    }
+
+}
+
+function drawGraph (canvas, array) {
+    var c = document.getElementById(canvas);
+    var context = c.getContext("2d");
+
+    printArray(array);
+    makeGraph();
+
+    //rysowanie punktów na canvas
+    context.fillStyle = "#000000";
+    for (var i = 0; i < map.length; i++) {
+        context.beginPath();
+        context.arc(coords[0][i], coords[1][i], 2, 0, 2*Math.PI);
+        context.closePath();
+        context.font = "10px Arial";
+        context.fillText("v"+i,coords[0][i] - 5,coords[1][i] - 5);
+        context.fill();
+        context.stroke();
+    }
+
+    //rysowanie linii między punktami
+    for (var i = 0; i < map.length; i++) {
+        for (var j = i+1; j < map[i].length; j++) {
+            if (map[i][j] == 1) {
+                context.moveTo(coords[0][i],coords[1][i]);
+                context.lineTo(coords[0][j],coords[1][j]);
+                context.stroke();
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //należy założyć, że każde dwa kolejne wiersze łączą się w co najmniej jednym punkcie
 //ten sam warunek dotyczy kolumn
@@ -48,20 +120,27 @@ function fillArray() {
 
 
 
-function checkConnections() {
-    if (checkRows() && checkColumns())
-        return true;
-    else
-        return false;
-}
+/*function checkConnections() {
+ if (checkRows() && checkColumns())
+ return true;
+ else
+ return false;
 
-//sprawdza czy kazde dwa kolejne wiersze maja punkt laczacy
-function checkRows() {
+//sprawdza czy kazde dwa kolejne wiersze maja punkt laczacy (bez sensu)
+/*(function checkRows() {
     var innerGuard = false;
+    var temp = 0;
     //var outerGuard = false;
     for (var i = 1; i < map.length; i++) {
         innerGuard = false;
-        for (var j = 0; j < map.length; j++) {
+        //if (!checkRow(i)) {
+            temp = Math.floor(Math.random()) * map.length;
+            map[i][temp] = 1;
+
+        }
+    //}
+
+       /* for (var j = 0; j < map.length; j++) {
             if (map[i][j] == map[i - 1][j]) {
                 innerGuard = true;
                 break;
@@ -74,6 +153,16 @@ function checkRows() {
     return true;
 }
 
+function checkRow(i) {
+    for (var j = 0; j < map.length; j++) {
+        if (map[i][j] == map[i - 1][j]) {
+            return true;
+        }
+    }
+    return false;
+}
 function checkColumns() {
     return true;
 }
+*/
+
