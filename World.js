0e0a2200279e31f728462c72f8c2f0235e07893d
MatPiw <mateusz.piwecki@wp.pl>
2014-10-18 19:34:16 +0200

    Poprawiony wygląd, dodane parę funkcji do sprawdzania ciągłości ścieżki

--- World.js
+++ World.js
@@ -12,21 +12,68 @@ function createArray(x, y) {
     return map;
 }
 
-var map = createArray(3,2);
-//przykładowa tablica 3x2
-map[0][0] = 123;
-map[0][1] = 456;
-map[1][0] = 2;
-map[1][1] = 89;
-map[2][0] = 67;
-map[2][1] = 7316;
+var map = createArray(10,10);
+
 
 
 function printArray(name) {
-    for (var i = 0; i < name.length; i++) {
-        for (var j = 0; j < name[i].length + 1; j++) {
-            document.getElementById(name).innerHTML += map[i][j] + " ";
+    fillArray();
+    document.getElementById(name).innerHTML = "";
+    for (var i = 0; i < map.length; i++) {
+        for (var j = 0; j < map[i].length; j++) {
+            if (map[i][j] == 1)
+                document.getElementById(name).innerHTML += "<b>" + map[i][j]+ "</b> ";
+            else
+                document.getElementById(name).innerHTML += map[i][j]+ " ";
         }
         document.getElementById(name).innerHTML += "<br>";
     }
+}
+
+function fillArray() {
+    var temp;
+    for (var i = 0; i < map.length; i++) {
+        for (var j = 0; j < map[i].length; j++) {
+            if (Math.random() < 0.4)
+                map[i][j] = 0;
+            else
+                map[i][j] = 1;
+        }
+    }
+}
+
+//należy założyć, że każde dwa kolejne wiersze łączą się w co najmniej jednym punkcie
+//ten sam warunek dotyczy kolumn
+//tylko, że robienie tego pętlami for będzie nieefektywne (dwie pętle for zagnieżdżone podwójnie)
+
+
+
+function checkConnections() {
+    if (checkRows() && checkColumns())
+        return true;
+    else
+        return false;
+}
+
+//sprawdza czy kazde dwa kolejne wiersze maja punkt laczacy
+function checkRows() {
+    var innerGuard = false;
+    //var outerGuard = false;
+    for (var i = 1; i < map.length; i++) {
+        innerGuard = false;
+        for (var j = 0; j < map.length; j++) {
+            if (map[i][j] == map[i - 1][j]) {
+                innerGuard = true;
+                break;
+            }
+        }
+            if (!innerGuard) {
+                return false;
+            }
+        }
+    return true;
+}
+
+function checkColumns() {
+    return true;
 }
