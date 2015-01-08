10855c910cf5c25469742712fcc509e1107af37f
lysy <lysy@debian>
2015-01-08 01:47:15 +0100

    update

--- move_by_direction.js
+++ move_by_direction.js
@@ -18,13 +18,39 @@ function moveDirection() {
 	} else {
 	    if (pastPositionTaxiInArrayX > positionTaxiInArrayX) {
 		if (direction=="left") {
-		    console.log("dol");
-		} else if (direction=="right") {
 		    console.log("^");
+		} else if (direction=="right") {
+		    console.log("tu");
 		}
 	    } else {
 		if (direction=="left") {
 		    console.log("^");
+
+		    tempX=Number(positionTaxiInArrayX);
+		    tempY=Number(positionTaxiInArrayY);
+		    
+		    while (world[tempY-1][tempX]!=1) {
+			tempX=tempX+1;
+		    }
+		    console.log(tempY-1, tempX);
+    document.getElementById("check").innerHTML = "";
+    for (var i = 0; i < world.length; i++) {		
+        for (var j = 0; j < world[i].length; j++) {
+            if (world[i][j] != 0)
+		if (i == tempY-1 && j == tempX) {
+		    document.getElementById("check").innerHTML += "<b><font color=red>" + world[i][j]+ "</font></b> ";
+		} else {
+                    document.getElementById("check").innerHTML += "<b>" + world[i][j]+ "</b> ";
+		}
+	    else
+		if (i == Number(positionTaxiInArrayY)+1 && j == positionTaxiInArrayX) {
+		    document.getElementById("check").innerHTML += "<b><font color=green>" + world[i][j]+ "</font></b> ";
+		} else {
+                    document.getElementById("check").innerHTML += " " + world[i][j]+ " ";
+		}
+	}
+        document.getElementById("check").innerHTML += "<br>";
+    }		    
 		} else if (direction=="right") {
 		    console.log("dol");
 		}
