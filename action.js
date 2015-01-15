67205566832045ee3ffdf1527e12ca03888d2316
lysy <lysy@debian>
2015-01-15 02:43:21 +0100

    gueue alpha

--- action.js
+++ action.js
@@ -1,5 +1,18 @@
+var gueue = new Array;
+
+function action_2() {
+    var check=gueue.shift();
+    if (check) {
+	console.log("wchodze");
+	checkDirection(check);
+    }
+    
+}
+
+
 function action() {
 
+    
     var input = document.getElementById("kolejka");
     
     var commands = String(input.value);
@@ -29,17 +42,28 @@ function action() {
 	    showPath(Number(tempX), Number(tempY));
 	    canvasAnimation();
 	} else if (command=="left") {
-	    checkDirection(command);
+	    gueue.push("left");
+//	    checkDirection(command);
 	    command = "";
 	} else if (command=="right") {
-	    checkDirection(command);
+	    gueue.push("right");
+//	    checkDirection(command);
+	    command = "";
+	} else if (command=="forward") {
+	    gueue.push("forward");
+//	    checkDirection(command);
+	    command = "";
+	} else if (command=="back") {
+	    gueue.push("back");
+//	    checkDirection(command);
 	    command = "";
 	}
-
     }
     
 
 }
+
+
 
 
 function checkDirection(direction) {
@@ -65,7 +89,49 @@ function checkDirection(direction) {
 		    showPath(Number(tempY), Number(tempX+1));
 		    canvasAnimation();
 
+		} else if (direction=="forward") {
+
+		    var tempX=Number(positionTaxiInArrayX);
+		    var tempY=Number(positionTaxiInArrayY);
+		    if (tempX+1==20) {
+			while (world[tempY][tempX-1]!=1) {
+			    tempY=tempY-1;
+			}			
+		    } else if (tempX-1==-1) {
+			while (world[tempY][tempX+1]!=1) {
+			    tempY=tempY-1;
+			}
+		    } else {
+			while (world[tempY][tempX+1]!=1 || world[tempY][tempX-1]!=1) {
+			    tempY=tempY-1;
+			}
+		    }
+
+		    showPath(Number(tempY-1), Number(tempX));
+		    canvasAnimation();
+		    
+		} else if (direction=="back") {
+
+		    var tempX=Number(positionTaxiInArrayX);
+		    var tempY=Number(positionTaxiInArrayY);
+		    if (tempX+1==20) {
+			while (world[tempY][tempX-1]!=1) {
+			    tempY=tempY+1;
+			}			
+		    } else if (tempX-1==-1) {
+			while (world[tempY][tempX+1]!=1) {
+			    tempY=tempY+1;
+			}
+		    } else {
+			while (world[tempY][tempX+1]!=1 || world[tempY][tempX-1]!=1) {
+			    tempY=tempY+1;
+			}
+		    }
+
+		    showPath(Number(tempY+1), Number(tempX));
+		    canvasAnimation();		    
 		}
+			 
 	    } else {
 		if (direction=="left") {
 
@@ -78,7 +144,7 @@ function checkDirection(direction) {
 		    canvasAnimation();
 		    		    
 		} else if (direction=="right") {
-		  
+		 
 		    var tempX=Number(positionTaxiInArrayX);
 		    var tempY=Number(positionTaxiInArrayY);
 		    while (world[tempY][tempX-1]!=1) {
@@ -87,6 +153,45 @@ function checkDirection(direction) {
 		    showPath(Number(tempY), Number(tempX-1));
 		    canvasAnimation();
 
+		} else if (direction=="forward") {
+
+		    var tempX=Number(positionTaxiInArrayX);
+		    var tempY=Number(positionTaxiInArrayY);
+		    if (tempX+1==20) {
+			while (world[tempY][tempX-1]!=1) {
+			    tempY=tempY+1;
+			}			
+		    } else if (tempX-1==-1) {
+			while (world[tempY][tempX+1]!=1) {
+			    tempY=tempY+1;
+			}
+		    } else {
+			while (world[tempY][tempX+1]!=1 || world[tempY][tempX-1]!=1) {
+			    tempY=tempY+1;
+			}
+		    }
+		    showPath(Number(tempY+1), Number(tempX));
+		    canvasAnimation();
+		} else if (direction=="back") {
+
+		    var tempX=Number(positionTaxiInArrayX);
+		    var tempY=Number(positionTaxiInArrayY);
+		    if (tempX+1==20) {
+			while (world[tempY][tempX-1]!=1) {
+			    tempY=tempY-1;
+			}			
+		    } else if (tempX-1==-1) {
+			while (world[tempY][tempX+1]!=1) {
+			    tempY=tempY-1;
+			}
+		    } else {
+			while (world[tempY][tempX+1]!=1 || world[tempY][tempX-1]!=1) {
+			    tempY=tempY-1;
+			}
+		    }
+
+		    showPath(Number(tempY-1), Number(tempX));
+		    canvasAnimation();		    
 		}
 	    }
 	} else {
@@ -111,6 +216,44 @@ function checkDirection(direction) {
 		    showPath(Number(tempY-1), Number(tempX));
 		    canvasAnimation();
 
+		} else if (direction=="forward") {
+
+		    var tempX=Number(positionTaxiInArrayX);
+		    var tempY=Number(positionTaxiInArrayY);
+		    if (tempY+1==20) {
+			while (world[tempY-1][tempX]!=1) {
+			    tempX=tempX-1;
+			}			
+		    } else if (tempY-1==-1) {
+			while (world[tempY+1][tempX]!=1) {
+			    tempX=tempX-1;
+			}
+		    } else {
+			while (world[tempY-1][tempX]!=1 || world[tempY+1][tempX]!=1) {
+			    tempX=tempX-1;
+			}
+		    }
+		    showPath(Number(tempY), Number(tempX-1));
+		    canvasAnimation();
+		} else if (direction=="back") {
+
+		    var tempX=Number(positionTaxiInArrayX);
+		    var tempY=Number(positionTaxiInArrayY);
+		    if (tempY+1==20) {
+			while (world[tempY-1][tempX]!=1) {
+			    tempX=tempX+1;
+			}			
+		    } else if (tempY-1==-1) {
+			while (world[tempY+1][tempX]!=1) {
+			    tempX=tempX+1;
+			}
+		    } else {
+			while (world[tempY-1][tempX]!=1 || world[tempY+1][tempX]!=1) {
+			    tempX=tempX+1;
+			}
+		    }
+		    showPath(Number(tempY), Number(tempX+1));
+		    canvasAnimation();		    
 		}
 	    } else {
 		if (direction=="left") {
@@ -132,11 +275,55 @@ function checkDirection(direction) {
 		    }
 		    showPath(Number(tempY+1), Number(tempX));
 		    canvasAnimation();
-		    		  document.getElementById("check").innerHTML = "";
+	
+		} else if (direction=="forward") {
+
+		    var tempX=Number(positionTaxiInArrayX);
+		    var tempY=Number(positionTaxiInArrayY);
+		    if (tempY+1==20) {
+			while (world[tempY-1][tempX]!=1) {
+			    tempX=tempX+1;
+			}			
+		    } else if (tempY-1==-1) {
+			while (world[tempY+1][tempX]!=1) {
+			    tempX=tempX+1;
+			}
+		    } else {
+			while (world[tempY-1][tempX]!=1 || world[tempY+1][tempX]!=1) {
+			    tempX=tempX+1;
+			}
+		    }
+		    showPath(Number(tempY), Number(tempX+1));
+		    canvasAnimation();
+		} else if (direction=="back") {
+
+		    var tempX=Number(positionTaxiInArrayX);
+		    var tempY=Number(positionTaxiInArrayY);
+		    if (tempY+1==20) {
+			while (world[tempY-1][tempX]!=1) {
+			    tempX=tempX-1;
+			}			
+		    } else if (tempY-1==-1) {
+			while (world[tempY+1][tempX]!=1) {
+			    tempX=tempX-1;
+			}
+		    } else {
+			while (world[tempY-1][tempX]!=1 || world[tempY+1][tempX]!=1) {
+			    tempX=tempX-1;
+			}
+		    }
+		    showPath(Number(tempY), Number(tempX-1));
+		    canvasAnimation();		    
+		}
+	    }
+	}
+}
+    
+/*		    document.getElementById("check").innerHTML = "";
     for (var i = 0; i < world.length; i++) {		
         for (var j = 0; j < world[i].length; j++) {
             if (world[i][j] != 0)
-		if (i == tempY+1 && j == tempX) {
+		if (i == tempY-1 && j == tempX) {
 		    document.getElementById("check").innerHTML += "<b><font color=red>" + world[i][j]+ "</font></b> ";
 		} else {
                     document.getElementById("check").innerHTML += "<b>" + world[i][j]+ "</b> ";
@@ -146,9 +333,4 @@ function checkDirection(direction) {
 		
 	}
         document.getElementById("check").innerHTML += "<br>";
-    }
-		}
-	    }
-	}
-}
-    
+    } */
