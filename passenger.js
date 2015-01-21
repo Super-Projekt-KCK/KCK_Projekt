937f60b949d0dec2bb7a6fab0128dea2008e6425
MatPiw <mateusz.piwecki@wp.pl>
2015-01-21 23:13:26 +0100

    Pasażer v. 0,000000002

--- passenger.js
+++ passenger.js
@@ -1,22 +1,26 @@
-var Passenger = function () {               //klasa pasażera (full obiektowość xD)
+var Passenger = function () {                            //klasa pasażera (full obiektowość xD)
     this.img = new Image();
     this.imgPath = "images/person.png";
-    this.background = {};               //żeby po wymazaniu było co przywrócić
-    this.position = new Array();                     //pozycja w tablicy
-    this.destination = new Array();                 //cel podróży  (nie wiedziałem czy string czy array więc dałem array)
+    this.background = {};                                //żeby po wymazaniu było co przywrócić
+    this.position = new Array();                         //pozycja w tablicy
+    this.destinationArray = new Array();                      //cel podróży  (nie wiedziałem czy string czy array więc dałem array)
+    this.destinationStreet = "";
     this.img.src = this.imgPath;
-    this.position[0] = Math.floor(Math.random() * size);
-    this.position[1] = Math.floor(Math.random() * size);
+    this.setRandomPosition();
+    this.setRandomDestination();
 }
 
-Passenger.prototype.randomPosition = function() {
-    this.position[0] = Math.floor(Math.random() * size);
-    this.position[1] = Math.floor(Math.random() * size);
+
+//-----------------------właściwości i pozycje------------------------------------//
+
+Passenger.prototype.setRandomPosition = function() {               //stawia ludka tylko na ulicy
+    do {
+        this.position[0] = Math.floor(Math.random() * size);
+        this.position[1] = Math.floor(Math.random() * size);
+    } while (world[this.position[1]][this.position[0]] != 1);               //znowu odwrócone współrzędne :C
 }
 
 Passenger.prototype.setPosition = function(x, y) {
-    //tutaj ma losować z jedynek na tablicy jakieś pole
-
     this.position[0] = x;
     this.position[1] = y;
 }
@@ -24,6 +28,14 @@ Passenger.prototype.setPosition = function(x, y) {
 Passenger.prototype.getPosition = function() {
     return this.position;
 }
+
+Passenger.prototype.setRandomDestination = function() {
+    this.destinationStreet = streetCoords.names[Math.floor(Math.random() * streetCoords.names.length)];
+    //trzeba dopisać warunek żeby nie chciał jechać tam gdzie stoi
+    speak ("My destination is " + this.destinationStreet + " street.");
+}
+
+//-------------------------rysowanie i wymazywanie---------------------------------//
 
 Passenger.prototype.draw = function() {
     //umieszczenie pamperka na mapie świata
@@ -33,31 +45,42 @@ Passenger.prototype.draw = function() {
     var context = canvas.getContext("2d");
 
     //zapamiętanie co było wcześniej
-    this.background = context.getImageData(this.position[0], this.position[1], 50, 50);
+    this.background = context.getImageData(this.position[0]*50, this.position[1]*50, 50, 50);
 
     //rysowanie pamperka na canvas
     context.drawImage(this.img, this.position[0] * 50, this.position[1] * 50);
 }
 
-Passenger.prototype.erase = function(x,y) {                                  //gdzie wymazać pasażera
+Passenger.prototype.erase = function(array) {                                  //gdzie wymazać pasażera
     var canvas = document.getElementById("town");
     var context = canvas.getContext("2d");
 
     //wymazanie z tablicy
-    world[this.position[0]][this.position[1]] = 1;
+    world[array[0]][array[1]] = 1;
 
     //wymazanie z canvas
-    context.putImageData(this.background, x, y);
+    context.putImageData(this.background, array[0]*50, array[1]*50);
 }
 
 
 
 //-----------------------poza obiektem-----------------------------------//
 
+
+//globalna tablica z pasażerami
+var passengers = [];
+var pIndex = 0;             //aktualna liczba pasażerów
+
 function testPerson() {
-    var person = new Passenger();
-    //person.setPosition(0,0);
-    person.draw();
-    speak("Where is the taxi?");
-    //person.erase(0,0);
+    passengers[pIndex] = new Passenger();
+    passengers[pIndex].draw();
+    pIndex++;
+    //speak("Where is the taxi?");
+}
+
+function clearPeople() {
+    for(var i = 0; i < passengers.length; i++) {
+        passengers[i].erase(passengers[i].position);
+    }
+    pIndex = 0;
 }
